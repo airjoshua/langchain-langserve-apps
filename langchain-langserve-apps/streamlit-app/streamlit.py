0b6e81a1fd5fed89e6709ca2d91238dae267be7c
Joshua Concepcion <concepcion@mac.com>
2024-04-06 21:39:54 -0700

    first commit

--- langchain-langserve-apps/streamlit-app/streamlit.py
+++ langchain-langserve-apps/streamlit-app/streamlit.py
@@ -1,5 +1,58 @@
 import streamlit as st
-from sales_chatbot.sales_chat import rag_chain
+from operator import itemgetter
+from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_core.pydantic_v1 import BaseModel
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+from langchain_pinecone import PineconeVectorStore
+
+load_dotenv()
+
+
+template = """
+You're a manager of a team of sales people. Write a helpful, inspiring e-mail to a sales person that will help them achieve their goals, based on the following context:
+
+{context}
+
+Please cite your sources.
+
+Question: {question}
+"""
+
+
+prompt = ChatPromptTemplate.from_template(template)
+
+
+llm = ChatOpenAI(name="gpt-4")
+
+vectorstore = PineconeVectorStore.from_existing_index(
+    index_name="sales",
+    embedding=OpenAIEmbeddings(model="text-embedding-3-large"),
+)
+
+
+retriever = vectorstore.as_retriever(
+    search_type="similarity",
+    search_kwargs={"k": 10},
+)
+
+
+rag_chain = (
+        RunnableParallel(
+            {"context": retriever,
+             "question": RunnablePassthrough()
+             })
+        | prompt
+        | llm
+        | StrOutputParser()
+)
+
+
+
+
+chain = rag_chain
 
 st.title('Sales-GPT')
 
