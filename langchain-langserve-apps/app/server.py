ffaf23451d60e279392a6ef6ee245f9a9df569ed
Joshua Concepcion <concepcion@mac.com>
2024-04-06 20:55:30 -0700

    first commit

---
+++ langchain-langserve-apps/app/server.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from langserve import add_routes
+from sales_chatbot import sales_chat
+
+load_dotenv()
+
+app = FastAPI()
+
+
+@app.get("/")
+async def redirect_root_to_docs():
+    return RedirectResponse("/docs")
+
+
+add_routes(
+    app,
+    sales_chat.chain,
+    path="/sales_bot",
+    playground_type="default",
+    enable_feedback_endpoint=True,
+    enable_public_trace_link_endpoint=True,
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+# gcloud run deploy [your-service-name] --source . --port 8001 --allow-unauthenticated --region us-central1 --set-env-vars=OPENAI_API_KEY=your_key
+
+# add_routes(app, rag_conversation_chain, path="/rag-conversation")
