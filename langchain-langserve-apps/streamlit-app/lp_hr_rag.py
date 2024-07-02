60e395f42f1ba08336ce4197f0ca5f927e57ce75
Joshua Concepcion <concepcion@mac.com>
2024-07-01 21:08:33 -0700

    lp hr

--- langchain-langserve-apps/streamlit-app/lp_hr_rag.py
+++ langchain-langserve-apps/streamlit-app/lp_hr_rag.py
@@ -1,16 +1,25 @@
 from enum import Enum
 from textwrap import dedent
-from typing import List
+from time import time
+from typing import Iterable, List
 
-import streamlit as st
 from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
+from langchain.retrievers.contextual_compression import ContextualCompressionRetriever
+from langchain_cohere import ChatCohere, CohereRerank
 from langchain_core.documents import Document
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.messages import AIMessage, AIMessageChunk
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnablePassthrough
+from langchain_core.runnables import (
+    RunnableGenerator,
+    RunnableParallel,
+    RunnablePassthrough,
+)
+from langchain_groq import ChatGroq
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
+import streamlit as st
 
 load_dotenv()
 
@@ -18,12 +27,65 @@ load_dotenv()
 class Prompts(Enum):
     rag = dedent(
         """
-        Answer the question based only on the following context. Cite the sources, page numbers, and urls of the information you used.
-        
-        Context: {context}
-        
+        {llama_3}You are an assistant that can answer questions about, and explain, LP's documents. Cite your sources, including
+        the 1) 'file name', 2) 'page number', and 3) 'url' that informed your answer. If you don't how to respond, just say that you don't know.
+        ############
+        LP's documents: {context}
+        ############
         Question: {question}
+        ############
+        Answer:
         """
+    )
+
+
+class LlamaPrompts(Enum):
+    rag = dedent(
+        """
+        <|begin_of_text|><|start_header_id|>system<|end_header_id|> You are an assistant that can answer questions about, and explain, LP's documents. Cite your sources, including
+        the 1) 'file name', 2) 'page number', and 3) 'url' that informed your answer. If you don't how to respond, 
+        just say that you don't know. <|eot_id|><|start_header_id|>user<|end_header_id|>
+        ############
+        Question: {question} 
+        ############
+        LP's documents: {context}
+        ############ 
+        Answer: <|eot_id|><|start_header_id|>assistant<|end_header_id|>
+    """
+    )
+    grader = dedent(
+        """
+        <|begin_of_text|><|start_header_id|>system<|end_header_id|> You are a grader assessing relevance 
+        of a retrieved document to a user question. If the document contains keywords related to the user question, 
+        grade it as relevant. It does not need to be a stringent test. The goal is to filter out erroneous retrievals. \n
+        Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question. \n
+        Provide the binary score as a JSON with a single key 'score' and no premable or explanation.
+         <|eot_id|><|start_header_id|>user<|end_header_id|>
+        Here is the retrieved document: \n\n {document} \n\n
+        Here is the user question: {question} \n <|eot_id|><|start_header_id|>assistant<|end_header_id|>
+    """
+    )
+    hallucination_grader = dedent(
+        """<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are a grader assessing whether 
+        an answer is grounded in / supported by a set of facts. Give a binary 'yes' or 'no' score to indicate 
+        whether the answer is grounded in / supported by a set of facts. Provide the binary score as a JSON with a 
+        single key 'score' and no preamble or explanation. <|eot_id|><|start_header_id|>user<|end_header_id|>
+        Here are the facts:
+        \n ------- \n
+        {documents} 
+        \n ------- \n
+        Here is the answer: {generation}  <|eot_id|><|start_header_id|>assistant<|end_header_id|>`
+    """
+    )
+    answer_grader = dedent(
+        """<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are a grader assessing whether an 
+        answer is useful to resolve a question. Give a binary score 'yes' or 'no' to indicate whether the answer is 
+        useful to resolve a question. Provide the binary score as a JSON with a single key 'score' and no preamble or explanation.
+         <|eot_id|><|start_header_id|>user<|end_header_id|> Here is the answer:
+        \n ------- \n
+        {generation} 
+        \n ------- \n
+        Here is the question: {question} <|eot_id|><|start_header_id|>assistant<|end_header_id|>"""
     )
 
 
@@ -36,40 +98,140 @@ def get_pinecone_retriever(index_name, embedding, k):
 
 
 def format_docs(docs: List[Document]) -> str:
-    """Convert Documents to a single string.:"""
-    formatted = [
+    formatted = (
         (
-            f"Source: {doc.metadata['file_name'].replace('-', ' ').replace('_', ' ')}\nURL: {doc.metadata['url']}"
+            f"File Name: {doc.metadata['file_name'].replace('-', ' ').replace('_', ' ')}\nURL: {doc.metadata['url']}"
             f"\nPage Number: {int(doc.metadata['page_number'])}\nContent: {doc.page_content if doc.metadata['category'] != 'Table' else doc.metadata['text_as_html']}"
         )
         for doc in docs
-    ]
+    )
+
     return "\n\n" + "\n\n".join(formatted)
 
 
-gpt_4o = init_chat_model(model="gpt-4o", model_provider="openai", temperature=0)
-prompt_template = ChatPromptTemplate.from_template(Prompts.rag.value)
-retriever = get_pinecone_retriever(
-    index_name="lp-hr-rag",
-    embedding=OpenAIEmbeddings(model="text-embedding-3-small"),
-    k=5,
-)
+def parse(ai_message) -> str:
+    """Parse the AI message."""
+    return ai_message.content.swapcase()
 
-rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt_template
-    | gpt_4o
-    | StrOutputParser()
-)
 
-st.title("LP HR")
+def streaming_parse(chunks: Iterable[AIMessage]) -> Iterable[str]:
+    for chunk in chunks:
+        yield chunk.content.swapcase()
+
+
+def get_rag_chain_from_docs():
+    retriever = get_pinecone_retriever(
+        index_name="lp",
+        embedding=OpenAIEmbeddings(model="text-embedding-3-small"),
+        k=5,
+    )
+    gpt_4o = init_chat_model(model="gpt-4o", model_provider="openai", temperature=0)
+    # streaming_parse = RunnableGenerator(streaming_parse)
+    rag_chain_from_docs = (
+        RunnablePassthrough.assign(context=(lambda x: format_docs(x["context"])))
+        | ChatPromptTemplate.from_template(Prompts.rag.value)
+        | gpt_4o
+        | StrOutputParser()
+    )
+
+    retrieve_docs = (lambda x: x["question"]) | retriever
+
+    # rag_chain = RunnablePassthrough.assign(context=retrieve_docs).assign(
+    #     answer=rag_chain_from_docs
+    # )
+
+    return RunnableParallel(
+        {"context": retriever, "question": RunnablePassthrough()}
+    ).assign(answer=rag_chain_from_docs)
+
+
+def get_basic_rag_chain(model):
+    gpt_4o = init_chat_model(
+        model="gpt-4o",
+        model_provider="openai",
+        temperature=0,
+    )
+    llama_3 = ChatGroq(
+        temperature=0,
+        model="llama3-70b-8192",
+    )
+    cohere = ChatCohere(model="command-r")
+    models = {"cohere": cohere, "GPT-4o": gpt_4o, "LLama 3": llama_3}
+    retriever = get_pinecone_retriever(
+        index_name="lp",
+        embedding=OpenAIEmbeddings(model="text-embedding-3-small"),
+        k=5,
+    )
+    compression_retriever = ContextualCompressionRetriever(
+        base_compressor=CohereRerank(),
+        base_retriever=retriever,
+    )
+    prompt = ChatPromptTemplate.from_template(
+        LlamaPrompts.rag.value if "llama_3" else Prompts.rag.value
+    )
+
+    return (
+        {
+            "context": compression_retriever | format_docs,  # compression_retriever
+            "question": RunnablePassthrough(),
+        }
+        | prompt
+        | models[model]
+        | StrOutputParser()
+    )
+
+
+def test_llm(chain):
+    questions = [
+        "What are the gift limits?",
+        "Give a thorough explanation of our dental benefits",
+        "Describe the medical plans.",
+        "What are the deductibles for the blue plan?",
+        "What services are not covered in the white plan?",
+        "Explain the different health plans available to salaried employees",
+        "Describe our dental benefits for salaried employees",
+        "what days are holidays?",
+        "Explain our personal leave policy",
+        "What is the hybrid work policy?",
+        "What's the out of pocket limit for the blue plan?",
+        "What is the deductible for the Orange Plan?",
+        "Can I relocate while still at LP?",
+        "I've witnessed bad behavior by a co-worker, but I am afraid of speaking out due to fear of retaliation. What are my protections?",
+    ]
+    for question in questions:
+        print(question)
+        print("\n")
+        print(chain.invoke(question))
+        print("################")
+
+
+def query_pc(query, index, embeddings, k=3):
+    vector_store = PineconeVectorStore(index=index, embeddings=embeddings)
+    return vector_store.similarity_search(
+        query=query,
+        k=k,
+    )
+
+
+#rag_chain = get_basic_rag_chain(model="llama_3")
+#test_llm(rag_chain)
+
+#print(rag_chain.invoke("What is the hybrid work policy?"))
+
+# response = rag_chain.invoke("What is the hybrid work policy?")
+
+
+st.title("LP")
 
 with st.chat_message("user"):
-    st.write("Hello 👋")
+    st.write("Ask about ")
 
-
+model_selection = st.selectbox(
+    "Pick an LLM",
+    ("GPT-4o", "LLama 3"))
+rag_chain = get_basic_rag_chain(model=model_selection)
+st.write("You chose:", model_selection)
 with st.form("my_form"):
-
     text = st.text_area(
         "Enter text:",
     )
--- langchain-langserve-apps/streamlit-app/unstructured_data_ingestion.py
+++ langchain-langserve-apps/streamlit-app/unstructured_data_ingestion.py
@@ -1,24 +1,37 @@
 import itertools as it
+import json
 import os
 import time
 from pathlib import Path
+from typing import List, Tuple
 
 from dotenv import load_dotenv
-from langchain.chat_models import init_chat_model
+from firecrawl import FirecrawlApp
+from langchain.schema import (
+    AIMessage,
+    BaseMessage,
+    HumanMessage,
+)
 from langchain_core.documents import Document
+
+# from langchain.chat_models import init_chat_model
+from langchain_experimental.text_splitter import SemanticChunker
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langserve.schema import CustomUserType
 from pinecone import Pinecone, ServerlessSpec
+from langchain_cohere import CohereEmbeddings
+from unstructured.staging.base import convert_to_dict
+from pydantic import Field
+from unstructured.chunking.basic import chunk_elements
+from unstructured.chunking.title import chunk_by_title
 from unstructured.staging.base import dict_to_elements
 from unstructured_client import UnstructuredClient
 from unstructured_client.models import shared
 from unstructured_client.models.errors import SDKError
 
 load_dotenv()
-
-
-gpt_4o = init_chat_model(model="gpt-4o", model_provider="openai", temperature=0)
 
 
 def create_pinecone_index(index_name, dimension):
@@ -41,7 +54,6 @@ def get_pinecone_client():
 
 
 def get_unstructured_client():
-    # EcxV76IXM7JZ6r5KtKqI63hOLXHGph
     return UnstructuredClient(
         api_key_auth=os.environ["UNSTRUCTURED_API_KEY"],
         server_url="https://api.unstructuredapp.biz/general/v0/general",
@@ -57,16 +69,19 @@ def get_unstructured_pdf_elements(file_path):
             content=f.read(),
             file_name=file_path,
         )
-    req = shared.PartitionParameters(
+    request = shared.PartitionParameters(
         files=files,
-        strategy="hi_res",
+        # strategy="fast",  # "hi_res",
+        strategy="hi_res",  # "hi_res",
         hi_res_model_name="yolox",
         pdf_infer_table_structure=True,
         skip_infer_table_types=[],
+        # extract_image_block_types=[],
+        # chunking_strategy="by_title",  # "'basic', 'by_page', 'by_similarity' or 'by_title'"
     )
     try:
-        resp = client.general.partition(request=req)
-        pdf_elements = dict_to_elements(resp.elements)
+        response = client.general.partition(request=request)
+        pdf_elements = dict_to_elements(response.elements)
     except SDKError as e:
         print(e)
     else:
@@ -74,41 +89,56 @@ def get_unstructured_pdf_elements(file_path):
         return pdf_elements
 
 
-def get_metadata(element):
+def get_metadata(element, parent_ids):
     metadata = element.metadata.to_dict()
     metadata["languages"] = ", ".join(metadata.pop("languages", ""))
     metadata["category"] = element.category
     metadata["url"] = "https://lpcorp.com/"
     metadata["file_name"] = Path(metadata.pop("filename")).stem
+    if metadata["category"] != "Title":
+        metadata["parent_title"] = parent_ids.get(metadata.get("parent_id", ""), "")
+
     return metadata
 
 
-def get_document(element):
-    metadata = get_metadata(element=element)
+def get_document(element, parent_ids_for_titles):
+    metadata = get_metadata(element=element, parent_ids=parent_ids_for_titles)
     return Document(page_content=element.text, metadata=metadata)
 
 
 def create_langchain_document(elements):
+    parent_ids_for_titles = {
+        element.id: element.text for element in elements if element.category == "Title"
+    }
     return [
-        get_document(element)
+        get_document(element, parent_ids_for_titles)
         for element in elements
-        if element.category not in ("Title")
+        if element.category not in ("Image")
     ]
 
 
-def get_splits(document):
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=20,
-        length_function=len,
-        is_separator_regex=False,
-    )
+def get_splits(document, semantic_chunking):
+    if semantic_chunking:
+        text_splitter = SemanticChunker(
+            OpenAIEmbeddings(model="text-embedding-3-small")
+        )
+    else:
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=20,
+            length_function=len,
+            is_separator_regex=False,
+        )
     langchain_document = create_langchain_document(document)
     return text_splitter.split_documents(langchain_document)
 
 
-def split_docs(documents):
-    return list(it.chain.from_iterable(get_splits(document) for document in documents))
+def split_docs(documents, semantic_chunking):
+    return list(
+        it.chain.from_iterable(
+            get_splits(document, semantic_chunking) for document in documents
+        )
+    )
 
 
 def add_records_to_pinecone_vs(documents, embedding, index_name):
@@ -119,24 +149,114 @@ def add_records_to_pinecone_vs(documents, embedding, index_name):
     )
 
 
-open_ai_embedding = OpenAIEmbeddings(model="text-embedding-3-small")
+def crawl_webpage(url):
+    app = FirecrawlApp(api_key=os.environ["FIRECRAWL_API_KEY"])
+    return app.crawl_url(
+        url=url,
+        params={
+            "crawlerOptions": {"limit": 10},
+            "pageOptions": {"onlyMainContent": True},
+        },
+        wait_until_done=False,
+    )
 
 
-pinecone_index = "lp-hr-rag"
+def scrape_url(url, llm_extraction=False):
+    app = FirecrawlApp(api_key=os.environ["FIRECRAWL_API_KEY"])
+    llm_params = {
+        "extractorOptions": {
+            "mode": "llm-extraction",
+            "extractionPrompt": "Based on the information on the page, extract the information from the schema. ",
+            "extractionSchema": {},
+        }
+    }
+    return app.scrape_url(
+        url=url,
+        params=llm_params if llm_extraction else {},
+    )
+
+
+def exclude_images(documents):
+    return [document for document in documents if document.category != "Image"]
+
+
+def convert_unstructured_to_csv(file_names, unstructured_docs):
+    unstructured_dicts = [
+        convert_to_dict(unstructured_doc) for unstructured_doc in unstructured_docs
+    ]
+    for file, dict_ in zip(file_names, unstructured_dicts):
+        file, *_ = file.name.split(".")
+        with open(
+            f"/Users/airjoshua/gen_ai/gen-ai/langchain/langchain-app/data/unstructured_docs{file}.json",
+            "w",
+        ) as write_file:
+            json.dump(dict_, write_file)
+
+
+def get_json_unstructured():
+    with open("data_file.json", "r") as read_file:
+        data = json.load(read_file)
+
+
+def upload_to_pinecone():
+    docs = split_docs(
+        documents=None,
+        semantic_chunking=False,
+    )
+
+    add_records_to_pinecone_vs(
+        documents=docs,
+        embedding=OpenAIEmbeddings(model="text-embedding-3-small"),
+        index_name="lp",
+    )
+
+
+def normalize_pdf():
+    files = list(
+        Path(
+            "/Users/airjoshua/gen_ai/gen-ai/langchain/langchain-app/data/manufacturing"
+        ).glob("*.pdf")
+    )
+    files = list(
+        Path("/Users/airjoshua/gen_ai/gen-ai/langchain/langchain-app/data").glob(
+            "*.pdf"
+        )
+    )
+
+    efficiency = get_unstructured_pdf_elements(
+        Path(
+            "/langchain/langchain-app/data/manufacturing/BreakdownOfEfficiencySectionOnDailyProductionReportv2.pdf"
+        )
+    )
+    pdfs = [get_unstructured_pdf_elements(pdf) for pdf in files]
+    chunked_pdfs = [
+        chunk_by_title(pdf, overlap=0, max_characters=1000, multipage_sections=False)
+        for pdf in pdfs
+    ]
+    element_chunking = [chunk_elements(pdf, max_characters=1000) for pdf in pdfs]
+    docs = split_docs(
+        documents=chunked_pdfs,
+        semantic_chunking=False,
+    )
+    add_records_to_pinecone_vs(
+        documents=docs,
+        embedding=OpenAIEmbeddings(model="text-embedding-3-small"),
+        index_name="lp",
+    )
+    no_images = [exclude_images(documents) for documents in pdfs]
+    title_chunks = chunk_by_title()
+    # how to post a job requistion
+
+    no_images_chunked_pdfs = [
+        chunk_by_title(pdf, overlap=50, max_characters=1000) for pdf in pdfs
+    ]
+
+
+# normalize_pdf()
+
+
+# pinecone_index = "lp"
 # create_pinecone_index(
 #     index_name=pinecone_index,
 #     dimension=1536,
 # )
-
-
-files = Path("").glob("*.pdf")
-# pdfs = [get_unstructured_pdf_elements(pdf) for pdf in pdfs]
-# docs = split_docs(pdfs)
-
-# %%
-# add_records_to_pinecone_vs(
-#     documents=docs, embedding=open_ai_embedding, index_name="lp-hr-rag"
-# )
-
-
-# docs = split_docs(unstructured_docs)
